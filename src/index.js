d50a17856659bb289332fab2478952fd0e933c66
Ben Firshman <ben@firshman.co.uk>
2017-09-11 11:24:31 +0300

    Darken theme and make bootstrap customizable
    ... by building from Sass

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <App />,
