ba759765d64e157f161663ab99114a678e1503fd
Ben Firshman <ben@firshman.co.uk>
2017-09-11 21:02:01 +0300

    Add Google Analytics tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
+import GoogleAnalytics from "react-ga";
 import { BrowserRouter, Route } from "react-router-dom";
 import ListPage from "./ListPage";
 import RunPage from "./RunPage";
+import config from "./config";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    if (config.GOOGLE_ANALYTICS_CODE) {
+      GoogleAnalytics.initialize(config.GOOGLE_ANALYTICS_CODE);
+    }
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <Route exact path="/" component={ListPage} />
           <Route exact path="/run/:rom" component={RunPage} />
+          <Route path="/" render={this.recordPageview} />
         </div>
       </BrowserRouter>
     );
   }
+
+  recordPageview = ({location}) => {
+    GoogleAnalytics.pageview(location.pathname);
+    return null;
+  };
 }
 
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
   BASE_ROM_URL: "https://d3cto2l652k3y0.cloudfront.net/",
+  GOOGLE_ANALYTICS_CODE: process.env.REACT_APP_GOOGLE_ANALYTICS_CODE,
 };
 
 export default config;
