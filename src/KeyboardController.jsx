f7bad3ceaee86723ed25ab0c7505deaca34133a5
Liam Newell <hibyguy@github.com>
2018-04-09 14:09:09 -0400

    prettier integration

--- src/KeyboardController.jsx
+++ src/KeyboardController.jsx
@@ -15,43 +15,43 @@ const KEYS = {
   105: [2, Controller.BUTTON_B], // Num-9
   99: [2, Controller.BUTTON_SELECT], // Num-3
   97: [2, Controller.BUTTON_START], // Num-1
-  104: [2, Controller.BUTTON_UP], // Num-8
+  104: [2, Controller.BUTTON_UP], // Num-8r
   98: [2, Controller.BUTTON_DOWN], // Num-2
   100: [2, Controller.BUTTON_LEFT], // Num-4
-  102: [2, Controller.BUTTON_RIGHT], // Num-6
+  102: [2, Controller.BUTTON_RIGHT] // Num-6
 };
 const ALT = {
-//Alternative Controls (Don't currently conflict with current ones)
+  //Alternative Controls (Don't currently conflict with current ones)
   //P1
-  70: [1,Controller.BUTTON_A], //F
-  71: [1,Controller.BUTTON_B], //G
-  87: [1,Controller.BUTTON_UP], //W
-  83: [1,Controller.BUTTON_DOWN], //S
-  65: [1,Controller.BUTTON_LEFT], //A
-  68: [1,Controller.BUTTON_RIGHT], //Date
+  70: [1, Controller.BUTTON_A], //F
+  71: [1, Controller.BUTTON_B], //G
+  87: [1, Controller.BUTTON_UP], //W
+  83: [1, Controller.BUTTON_DOWN], //S
+  65: [1, Controller.BUTTON_LEFT], //A
+  68: [1, Controller.BUTTON_RIGHT], //Date
   17: [1, Controller.BUTTON_SELECT], // Right Ctrl
   13: [1, Controller.BUTTON_START], // Enter
   //P2
   186: [2, Controller.BUTTON_A], // ;
   222: [2, Controller.BUTTON_B], // '
   191: [2, Controller.BUTTON_SELECT], // /
-  190: [2, Controller.BUTTON_START], // .
+  190: [2, Controller.BUTTON_START], // .r
   38: [2, Controller.BUTTON_UP], // Up
   40: [2, Controller.BUTTON_DOWN], // Down
   37: [2, Controller.BUTTON_LEFT], // Left
-  39: [2, Controller.BUTTON_RIGHT], // Right
-}
+  39: [2, Controller.BUTTON_RIGHT] // Right
+};
 
 export default class KeyboardController {
   constructor(options) {
     this.onButtonDown = options.onButtonDown;
     this.onButtonUp = options.onButtonUp;
-	this.altButtons = false;
+    this.altButtons = false;
   }
-  
+
   handleKeyDown = e => {
-	var key = null;
-	if(this.altButtons) key = ALT[e.keyCode];
+    var key = null;
+    if (this.altButtons) key = ALT[e.keyCode];
     else key = KEYS[e.keyCode];
     if (key) {
       this.onButtonDown(key[0], key[1]);
@@ -60,8 +60,8 @@ export default class KeyboardController {
   };
 
   handleKeyUp = e => {
-	var key = null;
-	if(this.altButtons) key = ALT[e.keyCode];
+    var key = null;
+    if (this.altButtons) key = ALT[e.keyCode];
     else key = KEYS[e.keyCode];
     if (key) {
       this.onButtonUp(key[0], key[1]);
@@ -72,8 +72,8 @@ export default class KeyboardController {
   handleKeyPress = e => {
     e.preventDefault();
   };
-  
+
   toggleControls = e => {
-      this.altButtons ? this.altButtons = false : this.altButtons = true;
+    this.altButtons ? (this.altButtons = false) : (this.altButtons = true);
   };
 }
