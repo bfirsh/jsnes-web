3318803a857281f2739d0fdfcd82af1a65bf43f1
Ben Firshman <ben@firshman.co.uk>
2019-01-19 19:14:14 -0800

    Handle ROM 404s

--- src/RunPage.js
+++ src/RunPage.js
@@ -14,6 +14,11 @@ function loadBinary(path, callback, handleProgress) {
   req.overrideMimeType("text/plain; charset=x-user-defined");
   req.onload = function() {
     if (this.status === 200) {
+      if (req.responseText.match(/^<!doctype html>/i)) {
+        // Got HTML back, so it is probably falling back to index.html due to 404
+        return callback(new Error("Page not found"));
+      }
+
       callback(null, this.responseText);
     } else if (this.status === 0) {
       // Aborted, so ignore error
@@ -153,7 +158,7 @@ class RunPage extends Component {
         path,
         (err, data) => {
           if (err) {
-            window.alert(`Error loading ROM: ${err.toString()}`);
+            this.setState({ error: `Error loading ROM: ${err.message}` });
           } else {
             this.handleLoaded(data);
           }
@@ -168,7 +173,7 @@ class RunPage extends Component {
         this.handleLoaded(reader.result);
       };
     } else {
-      window.alert("No ROM provided");
+      this.setState({ error: "No ROM provided" });
     }
   };
 
