7622364f395d7ac19b2737d96d9e649092ee4460
Anton Rissanen <anton.rissanen@reaktor.com>
2019-01-28 10:05:02 +0200

    Move ROM saving logic to RomLibrary module

--- src/ListPage.js
+++ src/ListPage.js
@@ -100,40 +100,7 @@ class ListPage extends Component {
       ? e.dataTransfer.items[0].getAsFile()
       : e.dataTransfer.files[0];
 
-    const reader = new FileReader();
-
-    const asHex = (buffer) => {
-      return Array.from(new Uint8Array (buffer))
-        .map (b => b.toString (16).padStart (2, "0"))
-        .join ("")
-    }
-
-    reader.onload = function(readFile) {
-      const byteString = readFile.target.result;
-      const ab = new ArrayBuffer(byteString.length)
-      var ia = new Uint8Array(ab)
-
-      for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-      }
-      const digest = crypto.subtle.digest('SHA-1', ab)
-      digest.then((buffer) => {
-        const hash = asHex(buffer)
-        const savedRomInfo = localStorage.getItem('savedRomInfo')
-        const existingLibrary = savedRomInfo ? JSON.parse(savedRomInfo) : []
-
-        const newRomInfo = JSON.stringify(existingLibrary.concat([{
-          name: file.name,
-          hash: hash,
-          added: Date.now()
-        }]))
-
-        localStorage.setItem('savedRomInfo', newRomInfo)
-        localStorage.setItem('blob-'+hash, readFile.target.result)
-      })
-    }
-
-    reader.readAsBinaryString(file);
+    RomLibrary.save(file)
   };
 }
 
