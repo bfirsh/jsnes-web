18d9e888eac9088eb09026a7812fbde4cd5bbbc5
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2022-08-24 22:13:24 -0700

    Add a switch to router to prevent duplication, fix navbar hovers

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
+          <Switch>
             <Route exact path="/">
               <ListPage />
             </Route>
@@ -41,6 +42,7 @@ class App extends Component {
               <RunPage />
             </Route>
             <Route path="/" element={this.recordPageview} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
