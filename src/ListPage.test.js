69b0f48fdc360b3f6e8a97d0b77dd90c1cbb32a5
Ben Firshman <ben@firshman.com>
2025-07-26 14:02:00 -0700

    format

--- src/ListPage.test.js
+++ src/ListPage.test.js
@@ -1,11 +1,11 @@
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
 import ListPage from "./ListPage";
 
 describe("ListPage", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    const root = createRoot(div)
+    const root = createRoot(div);
     root.render(
       <Routes location="/" context={{}}>
         <Route exact path="/" component={ListPage} />
--- src/RunPage.test.js
+++ src/RunPage.test.js
@@ -1,4 +1,4 @@
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { Routes, Route } from "react-router-dom";
 import RunPage from "./RunPage";
 
